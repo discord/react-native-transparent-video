51a334faacde822c7856e56e19404d679541e87d
emmyrlim <emmyrlim@gmail.com>
2023-08-08 09:48:21 -0700

    swift files

--- ios/TransparentVideoViewManager.swift
+++ ios/TransparentVideoViewManager.swift
@@ -17,20 +17,22 @@ class TransparentVideoView : UIView {
 
   private var source: VideoSource?
   private var playerView: AVPlayerView?
+  private var autoplay: Bool?
 
   @objc var src: NSDictionary = NSDictionary() {
     didSet {
       self.source = VideoSource(src)
+      self.autoplay = src["autoplay"] as? Bool ?? false
       let itemUrl = URL(string: self.source!.uri!)!
       loadVideoPlayer(itemUrl: itemUrl)
     }
   }
-  
+
   func loadVideoPlayer(itemUrl: URL) {
     if (self.playerView == nil) {
       let playerView = AVPlayerView(frame: CGRect(origin: .zero, size: .zero))
       addSubview(playerView)
-     
+
       // Use Auto Layout anchors to center our playerView
       playerView.translatesAutoresizingMaskIntoConstraints = false
       NSLayoutConstraint.activate([
@@ -39,7 +41,7 @@ class TransparentVideoView : UIView {
         playerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
         playerView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
       ])
-      
+
       // Setup our playerLayer to hold a pixel buffer format with "alpha"
       let playerLayer: AVPlayerLayer = playerView.playerLayer
       playerLayer.pixelBufferAttributes = [
@@ -47,32 +49,32 @@ class TransparentVideoView : UIView {
 
       // Setup looping on our video
       playerView.isLoopingEnabled = true
-      
+
       NotificationCenter.default.addObserver(self, selector: #selector(appEnteredBackgound), name: UIApplication.didEnterBackgroundNotification, object: nil)
       NotificationCenter.default.addObserver(self, selector: #selector(appEnteredForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
 
       self.playerView = playerView
     }
-    
+
     // Load our player item
     loadItem(url: itemUrl)
   }
-  
+
   deinit {
     playerView?.player?.pause()
     playerView?.player?.replaceCurrentItem(with: nil)
     playerView?.removeFromSuperview()
     playerView = nil
   }
-  
+
   // MARK: - Player Item Configuration
-  
+
   private func loadItem(url: URL) {
     setUpAsset(with: url) { [weak self] (asset: AVAsset) in
       self?.setUpPlayerItem(with: asset)
     }
   }
-  
+
   private func setUpAsset(with url: URL, completion: ((_ asset: AVAsset) -> Void)?) {
     let asset = AVAsset(url: url)
     asset.loadValuesAsynchronously(forKeys: ["metadata"]) {
@@ -90,7 +92,7 @@ class TransparentVideoView : UIView {
           }
       }
   }
-  
+
   private func setUpPlayerItem(with asset: AVAsset) {
     DispatchQueue.main.async { [weak self] in
       let playerItem = AVPlayerItem(asset: asset)
@@ -103,14 +105,18 @@ class TransparentVideoView : UIView {
         case .failure(let error):
           return print("Something went wrong when loading our video", error)
 
-        case .success(let player):
+        case .success(let player) where self?.autoplay == true:
           // Finally, we can start playing
           player.play()
+
+        case .success(let player):
+          // Finally, we can start playing
+          player.pause()
         }
       }
     }
   }
-  
+
   override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
     if keyPath == #keyPath(AVPlayerItem.status) {
       let status: AVPlayerItem.Status
@@ -132,7 +138,7 @@ class TransparentVideoView : UIView {
           }
       }
   }
-  
+
   func createVideoComposition(for asset: AVAsset) -> AVVideoComposition {
     let filter = AlphaFrameFilter(renderingMode: .builtInFilter)
     let composition = AVMutableVideoComposition(asset: asset, applyingCIFiltersWithHandler: { request in
@@ -149,9 +155,9 @@ class TransparentVideoView : UIView {
     composition.renderSize = asset.videoSize.applying(CGAffineTransform(scaleX: 1.0, y: 0.5))
     return composition
   }
-  
+
   // MARK: - Lifecycle callbacks
-  
+
   @objc func appEnteredBackgound() {
     if let tracks = self.playerView?.player?.currentItem?.tracks {
       for track in tracks {
