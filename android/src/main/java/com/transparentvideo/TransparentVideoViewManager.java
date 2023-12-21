7e1abede958b11fe6e9903484f27067f2aaddfa7
emmyrlim <emmyrlim@gmail.com>
2023-12-20 22:14:47 -0800

    logging

--- android/src/main/java/com/transparentvideo/TransparentVideoViewManager.java
+++ android/src/main/java/com/transparentvideo/TransparentVideoViewManager.java
@@ -79,21 +79,21 @@ public class TransparentVideoViewManager extends SimpleViewManager<LinearLayout>
     String file = src.getString("uri").toLowerCase();
     AlphaMovieView alphaMovieView = (AlphaMovieView)view.getChildAt(0);
     if (alphaMovieView == null) {
-      Log.d(TAG + "alpha movie view not found");
+      Log.i(TAG + "alpha movie view not found");
       alphaMovieView = new AlphaMovieView(reactContext, null);
       LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT);
       lp.gravity = Gravity.CENTER;
-      // alphaMovieView.setLayoutParams(lp);
+      alphaMovieView.setLayoutParams(lp);
       // alphaMovieView.setAutoPlayAfterResume(true);
       // alphaMovieView.setAutoPlayAfterInit(this.autoplay);
       // alphaMovieView.setLooping(this.loop);
-      Log.d(TAG + "alpha movie view added");
+      Log.i(TAG + "alpha movie view added");
       view.addView(alphaMovieView);
     } else {
-      Log.d(TAG + "alpha movie view found");
+      Log.i(TAG + "alpha movie view found");
     }
     Log.d(TAG + " setSrc", "file: " + file);
-    alphaMovieView.setLayoutParams(lp);
+    // alphaMovieView.setLayoutParams(lp);
     alphaMovieView.setAutoPlayAfterResume(true);
     alphaMovieView.setPacked(true);
     alphaMovieView.setVideoByUrl(file);
